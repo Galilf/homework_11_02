eed55e995d7e04fc748b4ef18a57af45aa08490c
Ilfat Windows <galinurov.ilfat@gmail.com>
2023-06-07 23:03:11 +0300

    change arguments in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,7 @@
-def text_upper(text):
+def text_upper(user_text):
     """Принимает строку и возвращает ее из заглавных букв"""
-    return text.upper()
+    return user_text).upper()
 
-def text_title(text):
+def text_title(user_text):
     """Принимает строку и возвращает ее c первой заглавной буквой в каждом слове"""
-    return text.title()
+    return user_text.title()
